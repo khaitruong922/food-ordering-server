60f19a65d2a6fc72d56a5752608e0aa22bc72d44
kasturez <tinhuynhvinh733@gmail.com>
2021-07-23 22:12:20 +0700

    Update update-category.dto.ts

--- src/category/dto/update-category.dto.ts
+++ src/category/dto/update-category.dto.ts
@@ -2,8 +2,4 @@ import { Optional } from '@nestjs/common';
 import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateCategoryDto } from './create-category.dto';
 
-export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
-    @ApiProperty()
-    products_id: number[]
-    
-}
+export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
