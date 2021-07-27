dc37b447211ac675b23bb3b40eb66453358be836
kasturez <tinhuynhvinh733@gmail.com>
2021-07-27 10:23:15 +0700

    first test for category

--- src/category/category.service.spec.ts
+++ src/category/category.service.spec.ts
@@ -5,15 +5,17 @@ import { CategoryService } from './category.service';
 import { Category } from './entities/category.entity';
 
 
-
 describe('CategoryService', () => {
   let service: CategoryService;
-
+  let getOne: jest.Mock;
   beforeEach(async () => {
+    getOne = jest.fn();
     const module: TestingModule = await Test.createTestingModule({
       providers: [CategoryService, {
         provide: getRepositoryToken(Category),
-        useValue: {}
+        useValue: {
+          getOne
+        }
       }],
     }).compile();
 
@@ -23,7 +25,17 @@ describe('CategoryService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
-  it('should show all the store that belong to food category', () => {
-    expect(service.getAll()).toEqual
-  });
+  describe('when the user is viewing all the store of a category', () =>{
+    let category: Category ={
+      name: "food",
+      stores: [],
+      id: 1
+    }
+    getOne.mockReturnValue(Promise.resolve(category))
+    it('should show all the store that belong to food category', async () => {
+      const fetchCategory = await service.getOne(1);
+      expect(fetchCategory).toEqual(category)
+    });
+  }) 
+  
 });
