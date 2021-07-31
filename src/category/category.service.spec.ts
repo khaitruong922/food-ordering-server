4e4272de21c1a18427b297dc73aaf5605979d5ef
kasturez <tinhuynhvinh733@gmail.com>
2021-07-31 09:14:53 +0700

    new repository mock

--- src/category/category.service.spec.ts
+++ src/category/category.service.spec.ts
@@ -1,18 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { randomInt, randomUUID } from 'crypto';
 import { Any, Repository } from 'typeorm';
 import { CategoryService } from './category.service';
 import { Category } from './entities/category.entity';
 
 export class mockCategoryRepositoryFake {
-  public create(): void {}
-  public save(): void{}
+  public create(): void { }
+  public async save(): Promise<void> { }
+  public async remove(): Promise<void> { }
+  public async findOne(): Promise<void> { }
 }
+
 
 describe('CategoryService', () => {
   let service: CategoryService;
   let serviceRepository: Repository<Category>;
- 
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CategoryService,
@@ -23,7 +27,7 @@ describe('CategoryService', () => {
       ],
     }).compile();
 
-    service = module.get<CategoryService>(CategoryService);
+    service = module.get(CategoryService);
     serviceRepository = module.get(getRepositoryToken(Category))
   });
 
@@ -32,11 +36,30 @@ describe('CategoryService', () => {
   });
 
   it('should create a new category and return that', async () => {
-    await service.create(
+
+    const savedCategory: Category = {
+      id: 1,
+      name: 'drink',
+      stores: [],
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }
+    const categoryRepositorySaveSpy = jest
+      .spyOn(serviceRepository, 'save')
+      .mockResolvedValue(savedCategory)
+
+    const categoryRepositoryfindOneSpy = jest
+    .spyOn(serviceRepository, 'findOne')
+    .mockResolvedValue(savedCategory)
+
+    const result = await service.create(
       {
         name: 'food'
       })
-    expect(await service.getOne(1))
+
+
+
+    expect(await service.getOne(2))
       .toEqual(
         {
           name: 'food',
