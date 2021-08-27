91042462a9cf175165e26e50fb2fb5eca7bfee76
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2021-08-27 14:31:43 +0700

    Update update-order.dto.ts

--- src/order/dto/update-order.dto.ts
+++ src/order/dto/update-order.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/swagger';
+import { IsEnum, IsOptional } from 'class-validator';
+import { OrderStatus } from '../entities/order.entity';
 import { CreateOrderDto } from './create-order.dto';
 
-export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
+export class UpdateOrderDto extends PartialType(CreateOrderDto) {
+    @IsEnum(OrderStatus)
+    @IsOptional()
+    status: OrderStatus
+}
